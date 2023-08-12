a24b4bc5b6b0e489e3c5c096dd63e0e0cd382e65
niyioo <niyiolagbegi@gmail.com>
2023-08-12 17:49:10 +0100

    base model

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#/usr/bin/python3
+""" Base model that defines the attributes for airbnb """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+
+    def save(self):
+        """Update updated_at with current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary representation of the instance."""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
+
+    def __str__(self):
+        """Return string representation of the instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
