ce1cbd15dc887d2ef75c559adfa2f3e3204b80ae
niyioo <niyiolagbegi@gmail.com>
2023-08-13 00:20:06 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        storage.new(self)
 
         if kwargs:
             for key, value in kwargs.items():
