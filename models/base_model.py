db72421ce234a51faede34a945a0961878523a72
niyioo <niyiolagbegi@gmail.com>
2023-08-12 18:36:01 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,8 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                setattr(self, key, value)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def save(self):
         """Update updated_at with current datetime."""
