701afb4dba8433b591b29741e989f629dd4b6969
niyioo <niyiolagbegi@gmail.com>
2023-08-13 19:44:49 +0100

    cc

--- console.py
+++ console.py
@@ -5,13 +5,19 @@ import cmd
 import shlex
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
     prompt = "(hbnb) "
 
-    l_classes = {
+    valid_classes = {
         "BaseModel": BaseModel,
         "User": User,
         "State": State,
@@ -34,16 +40,18 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Create a new instance of BaseModel and save it to JSON file."""
+        """Create a new instance of a class and save it to JSON file."""
         args = shlex.split(arg)
         if not args:
             print("** class name missing **")
             return
+
         class_name = args[0]
-        if class_name not in globals():
+        if class_name not in self.valid_classes:
             print("** class doesn't exist **")
             return
-        new_instance = globals()[class_name]()
+
+        new_instance = self.valid_classes[class_name](storage=storage)
         new_instance.save()
         print(new_instance.id)
 
@@ -53,16 +61,20 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
+
         class_name = args[0]
-        if class_name not in globals():
+        if class_name not in self.valid_classes:
             print("** class doesn't exist **")
             return
+
         if len(args) < 2:
             print("** instance id missing **")
             return
-        instances = storage.all()  # Replace with your actual storage
+
+        instances = storage.all()
         instance_id = args[1]
         key = class_name + "." + instance_id
+
         if key in instances:
             print(instances[key])
         else:
@@ -74,16 +86,20 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
+
         class_name = args[0]
-        if class_name not in globals():
+        if class_name not in self.valid_classes:
             print("** class doesn't exist **")
             return
+
         if len(args) < 2:
             print("** instance id missing **")
             return
-        instances = storage.all()  # Replace with your actual storage
+
+        instances = storage.all()
         instance_id = args[1]
         key = class_name + "." + instance_id
+
         if key in instances:
             instances.pop(key)
             storage.save()
@@ -91,23 +107,30 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_all(self, arg):
-        """
-        Prints all string representation of all instances based or not on the class name
-        """
+        """Print string representation of all instances."""
         args = shlex.split(arg)
-        objs = storage.all()
-        obj_list = []
-        if not arg:
-            for obj_key in objs:
-                obj_list.append(str(objs[obj_key]))
-        elif args[0] not in HBNBCommand.l_classes:
+        if args and args[0] not in self.valid_classes:
             print("** class doesn't exist **")
             return
+
+        if not args:
+            instances_list = []
+            for class_name in self.valid_classes:
+                instances_list.extend(
+                    [
+                        str(instance)
+                        for instance in storage.all(class_name).values()
+                    ]
+                )
+            print(instances_list)
         else:
-            for obj_key in objs:
-                if obj_key.split('.')[0] == args[0]:
-                    obj_list.append(str(objs[obj_key]))
-        print(obj_list)
+            class_name = args[0]
+            instances_list = [
+                str(instance)
+                for key, instance in storage.all(class_name).items()
+                if class_name in key
+            ]
+            print(instances_list)
 
     def do_update(self, arg):
         """Update an instance based on class name and id."""
@@ -115,30 +138,52 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
+
         class_name = args[0]
-        if class_name not in globals():
+        if class_name not in self.valid_classes:
             print("** class doesn't exist **")
             return
+
         if len(args) < 2:
             print("** instance id missing **")
             return
-        instances = storage.all()  # Replace with your actual storage
+
+        instances = storage.all()
         instance_id = args[1]
         key = class_name + "." + instance_id
+
         if key not in instances:
             print("** no instance found **")
             return
+
         if len(args) < 3:
             print("** attribute name missing **")
             return
+
         if len(args) < 4:
             print("** value missing **")
             return
+
         attr_name = args[2]
         attr_value = args[3]
         instance = instances[key]
         setattr(instance, attr_name, attr_value)
         instance.save()
+
+    def do_count(self, arg):
+        """Retrieve the number of instances of a class."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        instances_count = storage.count(class_name)
+        print(instances_count)
 
     def do_help(self, arg):
         """Display help information for commands."""
