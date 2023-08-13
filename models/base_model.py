8200e09cf13b11a402f8137fc4024d9d8844780a
niyioo <niyiolagbegi@gmail.com>
2023-08-13 20:11:41 +0100

    fx

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ parent class for airbnb """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -11,6 +12,8 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        storage.new()
+        storage.save()
 
         if kwargs:
             for key, value in kwargs.items():
@@ -22,7 +25,6 @@ class BaseModel:
     def save(self):
         """Update updated_at with current datetime."""
         self.updated_at = datetime.now()
-        from models import storage
         storage.save()
 
     def to_dict(self):
