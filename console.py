0c5d7868480e6a689ec99fcd732beb72404f43a5
niyioo <niyiolagbegi@gmail.com>
2023-08-13 20:07:27 +0100

    fx

--- console.py
+++ console.py
@@ -111,25 +111,16 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_all(self, arg):
-        """Print string representation of all instances."""
-        args = shlex.split(arg)
-        instances = storage.all()
+        """Print all instances"""
+        args = arg.split()
+        objs = storage.all()
         if not args:
-            instances_list = [
-                str(instance) for key, instance in instances.items()
-                if class_name in key
-            ]
-            print(instances_list)
+            print([str(objs[key]) for key in objs])
             return
-        class_name = args[0]
-        if class_name not in self.valid_classes:
+        if args[0] not in self.valid_classes:
             print("** class doesn't exist **")
             return
-        instances_list = [
-            str(instance) for key, instance in instances.items()
-            if class_name in key
-        ]
-        print(instances_list)
+        print([str(objs[key]) for key in objs if key.startswith(args[0])])
 
     def do_update(self, arg):
         """Update an instance based on class name and id."""
