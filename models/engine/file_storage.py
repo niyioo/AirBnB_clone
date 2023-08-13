e4172400405224dbc6d84251b548306ebe7249e0
niyioo <niyiolagbegi@gmail.com>
2023-08-13 22:36:15 +0100

    g

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,15 +37,10 @@ class FileStorage:
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
-        cls = {
-        "BaseModel": BaseModel,
-        "User": User,
-        "State": State,
-        "City": City,
-        "Amenity": Amenity,
-        "Place": Place,
-        "Review": Review
-        }
+        cls = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'City': City, 'Amenity': Amenity, 'State': State,
+               'Review': Review}
+        
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, 'r') as f:
                 for key, value in json.load(f).items():
