44fe1786a36636fc439b2a427fbc003f112c092c
niyioo <niyiolagbegi@gmail.com>
2023-08-13 20:18:14 +0100

    fx

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        from models import storage
-        storage.new()
-        storage.save()
 
         if kwargs:
             for key, value in kwargs.items():
