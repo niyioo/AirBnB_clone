e7cd74e148117ecb594591cdcdbe61c9923ec1d4
niyioo <niyiolagbegi@gmail.com>
2023-08-14 05:59:48 +0100

    n

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,55 +1,39 @@
 #!/usr/bin/python3
-"""Unit tests for Amenity class"""
+"""
+Unittest for amenity.py
+"""
 import unittest
 from models.amenity import Amenity
-from models import storage
-import sys
-import os
-
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(current_dir))
+import datetime
 
 
 class TestAmenity(unittest.TestCase):
-    """Test suite for Amenity class"""
+    """Tests instances and methods from amenity class"""
 
-    def setUp(self):
-        """Set up test instance"""
-        self.amenity = Amenity()
-        self.amenity.name = "Free Wi-Fi"
+    a = Amenity()
 
-    def tearDown(self):
-        """Clean up after test"""
-        storage.delete(self.amenity)
-        self.amenity = None
+    def test_class_exists(self):
+        """tests if class exists"""
+        res = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.a)), res)
 
-    def test_attributes(self):
-        """Test presence of attributes"""
-        self.assertTrue(hasattr(self.amenity, "name"))
+    def test_user_inheritance(self):
+        """test if Amenity is a subclass of BaseModel"""
+        self.assertIsInstance(self.a, Amenity)
 
-    def test_save(self):
-        """Test the save method"""
-        original_updated_at = self.amenity.updated_at
-        self.amenity.save()
-        self.assertNotEqual(original_updated_at, self.amenity.updated_at)
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.a, 'name'))
+        self.assertTrue(hasattr(self.a, 'id'))
+        self.assertTrue(hasattr(self.a, 'created_at'))
+        self.assertTrue(hasattr(self.a, 'updated_at'))
 
-    def test_to_dict(self):
-        """Test the to_dict() method of Amenity"""
-        amenity_dict = self.amenity.to_dict()
-        self.assertTrue(isinstance(amenity_dict, dict))
-        self.assertEqual(amenity_dict['__class__'], 'Amenity')
-        self.assertEqual(amenity_dict['name'], 'Free Wi-Fi')
+    def test_types(self):
+        """tests if the type of the attribute is the correct one"""
+        self.assertIsInstance(self.a.name, str)
+        self.assertIsInstance(self.a.id, str)
+        self.assertIsInstance(self.a.created_at, datetime.datetime)
+        self.assertIsInstance(self.a.updated_at, datetime.datetime)
 
-    def test_from_dict(self):
-        """Test the from_dict() method of Amenity"""
-        amenity_dict = self.amenity.to_dict()
-        new_amenity = Amenity(**amenity_dict)
-        self.assertEqual(new_amenity.id, self.amenity.id)
-        self.assertEqual(new_amenity.name, self.amenity.name)
-        self.assertEqual(new_amenity.created_at, self.amenity.created_at)
-        self.assertEqual(new_amenity.updated_at, self.amenity.updated_at)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
