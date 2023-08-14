df0dbc9f605311008a563e317e0e27133af2a0b7
niyioo <niyiolagbegi@gmail.com>
2023-08-14 05:51:19 +0100

    n

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,7 +14,7 @@ class TestBaseModel(unittest.TestCase):
     def setUp(self):
         """Set up test instance"""
         self.base_model = BaseModel()
-        self.base_model.name = "Test Model"
+        self.base_model.name = "Test Model"  # Set the name attribute
         self.storage = FileStorage()
         self.storage.new(self.base_model)
 
