c620ba03109f367c7935ce73704e3b8bdec40d1d
niyioo <niyiolagbegi@gmail.com>
2023-08-16 00:29:08 +0100

    fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" test for console """
+""" Test for console """
 import unittest
 from unittest.mock import patch
 from io import StringIO
@@ -7,9 +7,9 @@ from console import HBNBCommand
 from models import storage
 
 
-class TestConsoleShowCommand(unittest.TestCase):
-    """Test cases for the show command in the console"""
-    
+class TestConsoleUpdateCommand(unittest.TestCase):
+    """Test cases for the update command in the console"""
+
     def setUp(self):
         self.console = HBNBCommand()
         self.test_id = "7d3e1daf-ef32-42dd-ae75-d9ebdb7a8f9c"
@@ -19,37 +19,101 @@ class TestConsoleShowCommand(unittest.TestCase):
         self.console = None
         storage._FileStorage__objects = {}
 
-    def test_show_command(self):
-        """Test the show command with a stored object"""
+    def test_update_command(self):
+        """Test the update command by modifying a stored object"""
         storage._FileStorage__objects[f"BaseModel.{self.test_id}"] = self.obj
 
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd(f"show BaseModel {self.test_id}")
+            update_cmd = f"update BaseModel {self.test_id} name NewName"
+            self.console.onecmd(update_cmd)
             output = f.getvalue().strip()
 
-        self.assertIn("TestObject", output)
+        expected_value = storage._FileStorage__objects[
+            f"BaseModel.{self.test_id}"]["name"]
+        self.assertIn("NewName", expected_value)
 
-class TestConsoleDestroyCommand(unittest.TestCase):
-    """Test cases for the destroy command in the console"""
-    
+
+class TestConsoleCreateCommand(unittest.TestCase):
+    """Test cases for the create command in the console"""
+
     def setUp(self):
         self.console = HBNBCommand()
-        self.test_id = "7d3e1daf-ef32-42dd-ae75-d9ebdb7a8f9c"
-        self.obj = {"id": self.test_id, "name": "TestObject"}
 
     def tearDown(self):
         self.console = None
         storage._FileStorage__objects = {}
 
-    def test_destroy_command(self):
-        """Test the destroy command by removing a stored object"""
-        storage._FileStorage__objects[f"BaseModel.{self.test_id}"] = self.obj
-
+    def test_create_command(self):
+        """Test the create command by creating a new object"""
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd(f"destroy BaseModel {self.test_id}")
+            self.console.onecmd("create BaseModel")
             output = f.getvalue().strip()
 
-        self.assertNotIn(self.test_id, storage._FileStorage__objects)
+        self.assertIn("BaseModel.", output)
+        self.assertIn("id", storage._FileStorage__objects[output])
+        self.assertIsInstance(storage._FileStorage__objects[output], dict)
+
+
+class TestConsoleCountCommand(unittest.TestCase):
+    """Test cases for the count command in the console"""
+
+    def setUp(self):
+        self.console = HBNBCommand()
+
+    def tearDown(self):
+        self.console = None
+        storage._FileStorage__objects = {}
+
+    def test_count_command(self):
+        """Test the count command by counting stored objects"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("BaseModel.count()")
+            output = f.getvalue().strip()
+
+        self.assertEqual(int(output), 0)
+
+        # Create some objects
+        self.console.onecmd("create BaseModel")
+        self.console.onecmd("create BaseModel")
+        self.console.onecmd("create BaseModel")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("BaseModel.count()")
+            output = f.getvalue().strip()
+
+        self.assertEqual(int(output), 3)
+
+
+class TestConsoleAllCommand(unittest.TestCase):
+    """Test cases for the all command in the console"""
+
+    def setUp(self):
+        self.console = HBNBCommand()
+
+    def tearDown(self):
+        self.console = None
+        storage._FileStorage__objects = {}
+
+    def test_all_command(self):
+        """Test the all command by listing all stored objects"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("BaseModel.all()")
+            output = f.getvalue().strip()
+
+        self.assertEqual(output, "[]")
+
+        # Create some objects
+        self.console.onecmd("create BaseModel")
+        self.console.onecmd("create BaseModel")
+        self.console.onecmd("create BaseModel")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("BaseModel.all()")
+            output = f.getvalue().strip()
+
+        self.assertIn("BaseModel.", output)
+        self.assertIn("id", output)
+
 
 if __name__ == '__main__':
     unittest.main()
